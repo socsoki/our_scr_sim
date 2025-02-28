62758d18e3ca3d8cd27cad798fca98e8ca77dbe1
develomper <ya.mik4@ya.ru>
2025-02-28 22:07:32 +0300

    jtag_vpi module to tb

--- src/top.sv
+++ src/top.sv
@@ -2,33 +2,14 @@
 `timescale 1ns/1ps
 
 module top (
+    jtag_if jtag_curr_if,
     input clk,
     input rst
 );
 
-    // logic clk;
-    // logic rst;
-
     axil_if dmem_axil_if();
     axil_if imem_axil_if();
-    jtag_if jtag_curr_if();
 
-    jtag_vpi #(.DEBUG_INFO(0), .TCK_HALF_PERIOD(20*12))
-    jtag_vpi0 
-    (
-        .tms(jtag_curr_if.tms),
-        .tck(jtag_curr_if.tck),
-        .tdi(jtag_curr_if.tdi),
-        .tdo(jtag_curr_if.tdo),
-        .enable(1'b1),
-        .init_done(jtag_curr_if.trst_n)
-    );
-
-    initial begin
-        jtag_curr_if.trst_n = 1'b0; 
-        #100 jtag_curr_if.trst_n = 1'b1;
-
-    end
 
     scr1_top_axi_wrap our_scr1 (
         .dmem_axi_bus(dmem_axil_if),
--- tb/tb.sv
+++ tb/tb.sv
@@ -6,7 +6,20 @@ module testbench;
     logic clk;
     logic rst;
 
-    top top_with_scr1 (clk, rst);
+    jtag_if jtag_bus();
+
+    top top_with_scr1 (jtag_bus, clk, rst);
+
+    jtag_vpi #(.DEBUG_INFO(0), .TCK_HALF_PERIOD(20 * 12))
+    jtag_vpi0 
+    (
+        .tms(jtag_bus.tms),
+        .tck(jtag_bus.tck),
+        .tdi(jtag_bus.tdi),
+        .tdo(jtag_bus.tdo),
+        .enable(1'b1),
+        .init_done(jtag_bus.trst_n)
+    );
 
     initial begin
         forever begin
@@ -14,17 +27,18 @@ module testbench;
         end
     end
 
+    initial begin
+        jtag_curr_if.trst_n <= 1'b0; 
+        #100 jtag_curr_if.trst_n <= 1'b1;
+    end
+
     task reset();
         rst <= 1;
-        #(100*CLK_PERIOD);
+        #(100 * CLK_PERIOD);
         rst <= 0;
     endtask
 
     initial begin
-        fork
-            reset();
-        join_none
-        repeat(5000) @(posedge clk);
         reset();
     end
     
